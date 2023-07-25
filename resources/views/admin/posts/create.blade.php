df3c0a5995457e76e95ccf183001fa571c6a154e
Filip Szulżycki <filipszulzycki@interia.pl>
2023-07-25 09:44:45 +0200

    [WiP] Fix post creating screen

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -3,9 +3,9 @@
         <form method="POST" action="/admin/posts" enctype="multipart/form-data">
             @csrf
 
-            <x-form.input name="title"/>
-            <x-form.input name="slug"/>
-            <x-form.input name="thumbnail" type="file"/>
+            <x-form.input name="title" input="title"/>
+            <x-form.input name="slug" input="slug"/>
+            <x-form.input name="thumbnail" input="thumbnail" type="file"/>
             <x-form.textarea name="excerpt"/>
             <x-form.textarea name="body"/>
 
